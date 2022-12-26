94c868c67b22734732a50267025f00d7e0e1c9d1
Breno Carvalho <brenocarvalho709@gmail.com>
2022-12-26 10:54:10 -0300

    update baseUrl

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8000";
+// const baseUrl = "http://localhost:8000";
+const baseUrl = "http://15.229.15.44:8000";
 
 export default axios.create({
   baseURL: baseUrl,
